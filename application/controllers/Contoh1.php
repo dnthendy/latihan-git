3416cf53a36ec7586a7a561facc9ba05c10c47a5
dnthendy <hendydenata7@gmail.com>
2023-09-25 15:14:48 +0700

    ditambahkan oleh hendy

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Hendy Ramadhani Denata
+ Saya tinggal di daerah Kota Bambu
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
